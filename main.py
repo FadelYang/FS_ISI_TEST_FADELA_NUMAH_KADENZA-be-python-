fb0ae60234244b47a9cd58e176a717c6b928cb50
fadelanumah <fadelanumah@gmail.com>
2025-04-10 10:11:56 +0700

    fix: fix CORS issue

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 from app.controllers import todo_controller
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router( todo_controller.router, prefix="/api/v1")
+
+origins = ["*"]
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"]
+)
 
 @app.get("/")
 def read_root():
